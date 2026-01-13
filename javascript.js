7d82b8c3aa74e3b223ef976a99fdcd88249358c9
Rain <191729342+Goal-max@users.noreply.github.com>
2026-01-13 20:52:02 +1100

    working grid

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 32; i++) { 
+for (let i = 0; i < 256; i++) { 
   const div = document.createElement("div");
+  div.addEventListener ("mouseenter", change); 
   container.appendChild(div);
 }
  
+function change (event) {
+  event.target.style.backgroundColor = "red";
+  event.target.addEventListener("mouseleave", restore(event));
+}
+
+function restore (event) {
+  setTimeout((event) => {
+    event.target.style.backgroundColor = "white";
+    }, 10000);
+}
