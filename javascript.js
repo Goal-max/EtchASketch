04df5dc72acb2085adb99a57f81fba0836eb5c4a
Rain <191729342+Goal-max@users.noreply.github.com>
2026-01-14 20:14:16 +1100

    Alternate way to use setTimeout
    Learnt to store event object in variable so it can be passed into callback
    function.

--- javascript.js
+++ javascript.js
@@ -2,21 +2,14 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 256; i++) { 
   const div = document.createElement("div");
   div.addEventListener ("mouseenter", change); 
-  div.addEventListener("mouseleave", () => {
-    const obj = event.target;
-    setTimeout(() => {obj.style.backgroundColor = "white"}, 2000);
-  }); 
-  
+  div.addEventListener("mouseleave", restore);
   container.appendChild(div);
 }
  
 function change (event) {
   event.target.style.backgroundColor = "red";
 }
-/*
 function restore (event) {
-  let obj = event;
-  setTimeout((obj) => {
-  }, 10000);
+  let obj = event.target;
+  setTimeout(() => {obj.style.backgroundColor = "white"}, 1000);
 }
-*/
